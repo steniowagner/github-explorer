5599a3a19bbdb76b0c1f2f4307a5d142828195c8
STENIO FREITAS <stenio.wagner1@gmail.com>
2017-12-25 17:21:12 -0300

    Initial Setup

---
+++ App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+
+import './src/config/reactotron-config';
+
+const App = () => {
+  return (
+    <View>
+    </View>
+  );
+}
+
+export default App;

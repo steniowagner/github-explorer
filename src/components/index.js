eba69acf286244a4ad3540302b89ae1805b2ae24
STENIO FREITAS <stenio.wagner1@gmail.com>
2017-12-26 15:51:42 -0300

    Setting the first route depending if user is already logged

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,44 @@
-import React from 'react';
-import Routes from 'routes';
+import React, { Component } from 'react';
+import { AsyncStorage } from 'react-native';
+import createRootNavigator from 'routes';
 
-const AppContainer = () => (
-  <Routes />
-);
+class AppContainer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isUserAlreadyPersisted: false,
+      isUserAlreadyChecked: false
+    };
+  }
+
+  componentWillMount() {
+    //AsyncStorage.clear()
+    this.checkUserAlreadyPersisted()
+      .then(res => {
+        this.setState({
+          isUserAlreadyPersisted: res,
+          isUserAlreadyChecked: true
+        })
+      });
+  }
+
+  checkUserAlreadyPersisted = async () => {
+    const user = await AsyncStorage.getItem('@GitHubExplorer:username');
+    return user !== null;
+  }
+
+  render() {
+    if (!this.state.isUserAlreadyChecked) {
+      return null;
+    }
+
+    const ScreenLayout = createRootNavigator(this.state.isUserAlreadyPersisted);
+
+    return (
+      <ScreenLayout />
+    );
+  }
+}
 
 export default AppContainer;
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,20 +4,24 @@ import Repositories from './pages/repositories';
 import Organizations from './pages/organizations';
 import Welcome from './pages/welcome';
 
-const Routes = StackNavigator({
-  'Welcome': {
-    screen: Welcome
-  },
-  'Explorer': {
-    screen: TabNavigator({
-      Repositories: {
-        screen: Repositories
-      },
-      Organizations: {
-        screen: Organizations
-      }
-    })
-  }
-});
+const createRootNavigator = (userAlreadyPersisted = false) =>
+  StackNavigator({
+    'Welcome': {
+      screen: Welcome
+    },
+    'Explorer': {
+      screen: TabNavigator({
+        Repositories: {
+          screen: Repositories
+        },
+        Organizations: {
+          screen: Organizations
+        }
+      })
+    }
+  }, {
+      initialRouteName: userAlreadyPersisted ? 'Explorer' : 'Welcome',
+    }
+  );
 
-export default Routes;
+export default createRootNavigator;
